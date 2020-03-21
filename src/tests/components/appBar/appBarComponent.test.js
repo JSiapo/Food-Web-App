5e7bb4a818b4dd5ee30446a4ba8b22ebee63bc21
José Siapo <jsiapo@unitru.edu.pe>
2020-03-20 23:56:13 -0500

    Update test

--- src/tests/components/appBar/appBarComponent.test.js
+++ src/tests/components/appBar/appBarComponent.test.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import AppBar from 'components/appBar';
-import { daysObject } from 'utils/dateUtil';
 
 describe('appBar component', () => {
   it('should compare welcome message', () => {
     const appBar = render(<AppBar usuario='' />);
-    expect(appBar).toMatchSnapshot('Hello ');
+    expect(appBar).toMatchSnapshot();
   });
   it('should compare today', () => {
     const appBar = render(<AppBar usuario='' />);
-    expect(appBar).toMatchSnapshot(`
-    ${daysObject.daysName[0]}
-    ${daysObject.daysMonth[0]}
-    `);
+    expect(appBar).toMatchSnapshot();
   });
 });
